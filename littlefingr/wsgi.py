b69ccc7ec1bc737074da526575d9958de8207dbf
Vincent D'Onofrio <donofriov@gmail.com>
2017-01-31 17:16:12 -0500

    Initial Setup

---
+++ littlefingr/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for littlefingr project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "littlefingr.settings")
+
+application = get_wsgi_application()
